b4d95d921e44f2266ba0bedd9b177b73c96be68f
Abhishek-codesai <abhisharma272122@gmail.com>
2026-06-01 00:13:36 +0530

    Leetcode problem - 136 solution

---
+++ 03. Vector/03_Size_Capacity.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec;
+
+    vec.push_back(0);
+    vec.push_back(1);
+    vec.push_back(2);
+
+    cout<<"Vector size = "<<vec.size()<<endl;
+    cout<<"Vector capacity = "<<vec.capacity()<<endl;
+    return 0;
+}
