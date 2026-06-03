696a50bb8c0237415af317fde4e2691ffc42c2f7
Abhishek-codesai <abhisharma272122@gmail.com>
2026-06-03 22:35:27 +0530

    Day-5 files

---
+++ 03. Vector/08_PairSum_Optimal.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> PairSum(vector<int>nums, int target){
+    vector<int>ans;
+    int size = nums.size();
+    int i= 0, j= size-1;
+    int ps=0; //Pair Sum
+    while(i<j){
+        ps = nums[i]+nums[j];
+        if(ps > target) j--;
+        if(ps < target) i++;
+        if(ps == target){
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+}
+int main(){
+    vector<int>nums = {2,7,11,15};
+    int target = 9;
+    vector<int> ans = PairSum(nums, target);
+    cout<<ans[0]<<", "<<ans[1]<<endl;
+    return 0;
+}
