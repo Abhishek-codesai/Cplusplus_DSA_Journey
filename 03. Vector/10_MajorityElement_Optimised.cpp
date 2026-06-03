0a31006f810e2eceeabcc3cdbec0c284716a9ccd
Abhishek-codesai <abhisharma272122@gmail.com>
2026-06-04 00:09:33 +0530

    day 5 files

--- 03. Vector/10_MajorityElement_Optimised.cpp
+++ 03. Vector/10_MajorityElement_Optimised.cpp
@@ -1,17 +1,20 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int MajorityEle(vector<int>nums){
+    int n = nums.size();
     sort(nums.begin(), nums.end()); //sort
     int freq = 1, ans = nums[0];
-    for(int val: nums){
+    for(int i=1; i<n; i++){
         if(nums[i] == nums[i-1]){ freq++;}
         else {
             freq = 1;
-            ans = val;
+            ans = nums[i];
         }
         if(freq > n/2) return ans;
     }
+    return -1;
 }
 int main(){
     vector<int>nums = {2,1,1,2,2};
