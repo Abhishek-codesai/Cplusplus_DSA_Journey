2f1238356a41513773b52a77602eab0910f2f9cc
Abhishek-codesai <abhisharma272122@gmail.com>
2026-06-18 18:50:39 +0530

    optimized inner j loop

--- 03. Vector/07_PairSum_BruteForce.cpp
+++ 03. Vector/07_PairSum_BruteForce.cpp
@@ -5,7 +5,7 @@ vector<int> PairSum(vector<int>nums, int target){
     vector<int>ans;
     int size = nums.size();
     for(int i=0; i<size; i++){
-        for(int j=0; j<size; j++){
+        for(int j=i+1; j<size; j++){
             if(nums[i] + nums[j] == target){
                 ans.push_back(i);
                 ans.push_back(j);
